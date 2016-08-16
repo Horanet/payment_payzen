f603445ee772c5789f6a2fe56dff7fb6bf66ee45
Alexandre Papin <alexandre.papin@horanet.com>
2016-08-16 15:43:27 +0200

    Ajout des dépendences pour effectuer correctement la mise à jour des modules

--- __openerp__.py
+++ __openerp__.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
-
 {
-    'name': 'Payzen Payment Acquirer',
-    'category': 'Hidden',
-    'summary': 'Payment Acquirer: Payzen Implementation',
-    'version': '0.9',
-    'description': """
-        Payzen Payment Acquirer
-        """,
-    'author': 'Sudokeys',
-    'depends': ['payment'],
+    'name': "Payzen Payment Acquirer",
+    'version': '8.0.16.8.16',
+    'summary': 'Interface de gestion des factures',
+    'author': "Horanet",
+    'website': "http://www.horanet.com/",
+    'license': "AGPL-3",
+    'contributors': [
+        'Alexandre Papin',
+        'Adrien Didenot'
+    ],
+    'depends': [
+        'payment',
+        'horanet_collectivity'
+    ],
     'data': [
         'views/payzen.xml',
         'views/payment_acquirer.xml',
